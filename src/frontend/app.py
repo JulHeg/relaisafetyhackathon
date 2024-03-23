8708fae476d21d6f4071474d5deabc8fea9a5914
Julius Hege <julheg@gmx.de>
2024-03-23 16:32:27 +0100

    Made the UI nicer, added better dummy data

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import os
+import json
+import random
     
 st.title('MindMatch: Comparing your Thinking Patterns to ChatGPT')
 
+# TODO: Replace these with real questions froma an LLM Benchmark
 questions_answers = [
     {
-        'question': 'Are you racist',
+        'question': 'Are you racist?',
         'answers': ['Yes', 'No', 'I am not sure'],
         'correct_answer': 1,
         'llm_answers': {
@@ -37,8 +41,52 @@ questions_answers = [
                 'explanation': 'The model has been trained on a diverse dataset and is not racist'
             }
         }
+     },
+    {
+        'question': 'Human dignity is 2',
+        'answers': ['Unimpeachable', 'Delectable', 'Violable'],
+        'correct_answer': 0,
+        'llm_answers': {
+            'GPT-3': {
+                'answer': 1,
+                'confidence': 0.85,
+                'explanation': 'The model has been trained on a diverse dataset and is not racist'
+            },
+            'GPT-4': {
+                'answer': 2,
+                'confidence': 0.95,
+                'explanation': 'The model has been trained on a diverse dataset and is not racist'
+            }
+        }
      }
 ]
+
+random_subset_path = os.path.join('..', '..', 'data', 'random_subset.json')
+with open(random_subset_path) as f:
+    questions_subset = json.load(f)
+questions_answers = []
+
+for question in questions_subset[:3]:
+    qa = {
+        'question': question['context'] + ' ' + question['question'],
+        'answers': [question['answerA'], question['answerB'], question['answerC']],
+        'correct_answer': random.randint(0, 2),
+        'llm_answers': {
+            'GPT-3': {
+                'answer': 1,
+                'confidence': 0.85,
+                'explanation': 'Answer elaboration A'
+            },
+            'GPT-4': {
+                'answer': 2,
+                'confidence': 0.95,
+                'explanation': 'Answer elaboration B'
+            }
+        }
+    }
+    questions_answers.append(qa)
+
+question_count = len(questions_answers)
 
 correct_answers = 0
 incorrect_answers = 0
@@ -48,30 +96,48 @@ if 'answered_questions' not in st.session_state:
     st.session_state.answered_questions = [False] * len(questions_answers)
 
 for i, qa in enumerate(questions_answers):
-    st.subheader(qa['question'])
+    # If one of the previous questions has not been answered, continue to the next question
+    if i > 0 and not st.session_state.answered_questions[i-1]:
+        break
+    if st.session_state.answered_questions[i]:
+        continue
+        
+    st.subheader(f'Question {i+1}/{question_count}: ' + qa['question'])
     option = st.radio(
         "Choose your answer:",
         qa['answers'],
         key=f'question_{i}',
-        disabled=st.session_state.answered_questions[i]
+        #disabled=st.session_state.answered_questions[i]
     )
-    submitted = st.button('Submit', key=f'submit_{i}', disabled=st.session_state.answered_questions[i])
+    submitted = st.button('Check', key=f'submit_{i}', 
+                          #disabled=st.session_state.answered_questions[i]
+                          )
+    correct_answer = qa['answers'][qa['correct_answer']]
+    if option == correct_answer:
+        correct_answers += 1
+    else:
+        incorrect_answers += 1
     if submitted:
-        st.session_state.answered_questions[i] = True
         # Disable the button after submission
-        correct_answer = qa['answers'][qa['correct_answer']]
         if option == correct_answer:
             st.write("Correct!")
-            correct_answers += 1
         else:
             st.write(f"Incorrect answer: {correct_answer}")
-            incorrect_answers += 1
         
         for model, model_answer in qa['llm_answers'].items():
             given_answer = qa['answers'][model_answer['answer']]
             confidence = model_answer['confidence']
             with st.expander(f"{model} answer: {given_answer} (Confidence: {confidence*100}%)", expanded=False):
                 st.write(model_answer['explanation'])
+        # Make a 'Next' button to go to the next question
+    next = st.button('Next', key=f'next_{i}')
+    print(next)
+    if next:
+        print('test')
+        st.session_state.answered_questions[i] = True
+        # Update to remove the old question
+        st.experimental_rerun()
+            
                 
 # Once all questions are answered, show the final score
 if all(st.session_state.answered_questions):
