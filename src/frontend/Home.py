deb24cc1ea42f8c12ebdbbf2d244809d93f3f702
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2024-03-24 13:31:35 +0100

    Adding style recommendations.

--- src/frontend/Home.py
+++ src/frontend/Home.py
@@ -13,7 +13,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 700px !important; # Set the width to your desired value
+            width: 400px !important; # Set the width to your desired value
         }
         a[data-testid="stPageLink-NavLink"] {
             
